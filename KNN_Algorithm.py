e853e8fd4544db1be6890c7cf65952b973412a3c
Jean Paul <paulfromperu123@gmail.com>
2022-08-02 16:20:33 -0500

    Añadimos metodos para mostrar datos

--- KNN_Algorithm.py
+++ KNN_Algorithm.py
@@ -9,3 +9,13 @@ from sklearn.model_selection import train_test_split
 #Leer la dataset
 iris = pd.read_csv("iris.csv")
 iris.head(20)
+#puedes usar "iris.tail() para ver las ultimas 5 filas de tu dataset"
+
+iris.shape
+iris["species"].value_counts()
+#vemos las columnas y valores del dataset resumidas
+iris.columns
+iris.values
+#Pedimos un resumen de los datos del dataset
+iris.info()
+iris.describe(include='all')
